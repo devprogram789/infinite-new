030ac590182b2670d18c802d15a1f03fb7cbdf62
S.K <devprogram789@gmail.com>
2023-01-04 16:45:09 +0700

    up

--- src/components/WidgetWrapper/WidgetWrapper.js
+++ src/components/WidgetWrapper/WidgetWrapper.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function WidgetWrapper() {
   return (
     <>
-      <div class="fixed bottom-5 right-5">
+      <div class="fixed bottom-[8rem] md:bottom-5 lg:bottom-8 right-5">
         <div class="flex flex-col justify-center items-center text-center gap-2">
-          <div class="w-10 h-10 bg-infinite2-4 rounded-full p-1">
+          <div class="w-10 h-10 md:w-12 md:h-12 lg:w-12 lg:h-12 bg-infinite2-4 rounded-full p-1 shadow-lg">
             <a href="https://lin.ee/mlElpgq">
               <img
                 src="../../assets/images/footer/icon-line.png"
@@ -13,7 +13,7 @@ export default function WidgetWrapper() {
               />
             </a>
           </div>
-          <div class="w-10 h-10 bg-infinite2-4 rounded-full p-1">
+          <div class="w-10 h-10 lg:w-12 lg:h-12 bg-infinite2-4 rounded-full p-1 shadow-lg">
             <a href="https://www.facebook.com/InfiniteDigitalConsultants/">
               <img
                 src="../../assets/images/footer/icon-facebook.png"
@@ -21,7 +21,7 @@ export default function WidgetWrapper() {
               />
             </a>
           </div>
-          <div class="w-10 h-10 bg-infinite2-4 rounded-full p-1">
+          <div class="w-10 h-10 lg:w-12 lg:h-12 bg-infinite2-4 rounded-full p-1 shadow-lg">
             <a href="tel:+66918166689">
               <img
                 src="../../assets/images/footer/โทรศัพท์-mini.png"
